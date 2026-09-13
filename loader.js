f17d535b70b85872edbc13821efb39248f55c72d
nate smith <nsmith@archive.org>
2026-09-13 16:29:10 -0700

    fix linting issues in get_ruffle_files

--- loader.js
+++ loader.js
@@ -485,15 +485,15 @@ globalThis.Module = null;
      function get_ruffle_files(cfgr, metadata, modulecfg, filelist) {
        globalThis.RufflePlayer = globalThis.RufflePlayer || {};
        globalThis.RufflePlayer.config = modulecfg.config;
-       var files = [];
-       var meta = dict_from_xml(metadata);
-       var game_files = files_with_ext_from_filelist(filelist, meta.emulator_ext);
+       const files = [];
+       const meta = dict_from_xml(metadata);
+       const game_files = files_with_ext_from_filelist(filelist, meta.emulator_ext);
 
        if (game_files.length > 0) {
-         var file = game_files[0]; // only allow one .swf file to be loaded
-         var title = 'Downloading Game File';
-         var url = (file.name.includes('/')) ? get_zip_url(file.name)
-                                             : get_zip_url(file.name, get_item_name(game));
+         const file = game_files[0]; // only allow one .swf file to be loaded
+         const title = 'Downloading Game File';
+         const url = (file.name.includes('/')) ? get_zip_url(file.name)
+                                               : get_zip_url(file.name, get_item_name(game));
          files.push(cfgr.mountFile('/' + file.name, cfgr.fetchFile(title, url)));
          files.push(cfgr.swf_file_name('/' + file.name));
        }
